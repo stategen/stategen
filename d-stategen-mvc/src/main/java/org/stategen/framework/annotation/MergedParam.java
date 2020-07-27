11f93276913145e05de822790371255a03668649
niaoge <78493244@qq.com>
2020-07-27 16:37:14 +0800

    dao +requestParam

---
+++ d-stategen-mvc/src/main/java/org/stategen/framework/annotation/MergedParam.java
@@ -0,0 +1,11 @@
+package org.stategen.framework.annotation;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+import io.swagger.annotations.ApiParam;
+
+@ApiParam
+
+public @interface MergedParam {
+    
+}
